44a59ac63a9e67f9bbed407c14f4ded354dcad73
mateus-rodriguess <mateus18nc@gmail.com>
2026-03-11 01:32:50 -0300

    feat:[front-end] register routes pages

---
+++ frontend/src/routes/router.tsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Tasks from "../pages/Tasks";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import CreateCategory from "../pages/category/CreateCategory";
+import CreateTask from "../pages/Tasks/createTask";
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Tasks />} />
+      <Route path="/tasks/new" element={<CreateTask />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/categories/new" element={<CreateCategory />} />
+    </Routes>
+  );
+}
