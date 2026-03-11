01518fc460aeca9cc5736caaab1f66d2b671634b
mateus-rodriguess <mateus18nc@gmail.com>
2026-03-11 01:31:41 -0300

    fix:[front-end] change name router /tasks

--- frontend/src/pages/Tasks/index.tsx
+++ frontend/src/pages/Tasks/index.tsx
@@ -37,7 +37,7 @@ export default function Tasks() {
 
         <div className="flex gap-2">
           <Link
-            to="/task/new"
+            to="/tasks/new"
             className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition"
           >
             + Nova tarefa
